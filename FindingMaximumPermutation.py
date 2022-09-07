a8faaa2835e35676a98d2a778e45262e02a867a3
sleevmichael <okofumichael@example.com>
2022-09-07 15:54:45 +0100

    Forth commit

---
+++ FindingMaximumPermutation.py
@@ -0,0 +1,15 @@
+def max_perm(M):
+    n = len(M)
+    A = set(range(n))
+    count=[]*n
+    for i in M:
+        count[i] += 1
+    Q = [i for i in A if count[i] == 0]
+    while Q:
+        i = Q.pop()
+        A.remove(i)
+        j = M[i]
+        count[j] -= 1
+        if count[j] == 0:
+            Q.append(j)
+    return A
